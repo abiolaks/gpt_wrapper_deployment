50b14443414cceb3dc943c19552153ee253a84df
abiolaks <kingsleylawani@gmail.com>
2025-01-14 12:53:40 +0100

    add home script for streamline deployement

---
+++ src/home.py
@@ -0,0 +1,38 @@
+# streamlit deployement
+import streamlit as st
+
+import streamlit as st
+from gpt_wrapper import generate_text, generate_image
+
+# title of the app
+st.title("Brings Your Though and Imagination to Life- KING AI")
+
+# Header for the openai wrapper
+st.header("Text Generator")
+
+# text box for prompt
+open_ai_prompt = st.text_input("Enter your prompt")
+
+# button to send the prompt
+if st.button("Send"):  # check wether the button for our prompt is clicked
+    # gpt method text generation
+    generate_text(open_ai_prompt)
+    # success message
+    st.sucess("Content generated successfully")
+else:
+    # error message if prompt is not entered.
+    # st.error("Content generation failed")
+    st.warning("Please insert a prompt")
+
+st.divider()
+
+st.header("Image Generation")
+# variable to store the prompt
+openai_image_prompt = st.text_input("Enter your prompt", key=2)
+
+if st.button("Send", key=2):
+    # gpt method image generation
+    generate_image(openai_image_prompt)
+    st.success("Image generated successfully")
+else:
+    st.warning("Please insert a prompt to generate an image")
